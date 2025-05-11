34994907e64b86d129b997ddb136d70dd05a104e
Ahmad-7866 <mattremy90@gmail.com>
2025-05-11 20:15:20 +0800

    Update tambah-peserta.js

--- api/tambah-peserta.js
+++ api/tambah-peserta.js
@@ -3,13 +3,19 @@ export default async function handler(req, res) {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
   }
 
-  const data = req.body;
-  const response = await fetch('https://script.google.com/macros/s/AKfycbyUKPFBGT1zzOt0BLKb6E8tEp8DazCMnUJQ2ArFvWM0UmS0eTVsmuvRoSmHDQ41VX6r8g/exec', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-  });
+  try {
+    const data = req.body;
 
-  const result = await response.json();
-  return res.status(200).json(result);
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyUKPFBGT1zzOt0BLKb6E8tEp8DazCMnUJQ2ArFvWM0UmS0eTVsmuvRoSmHDQ41VX6r8g/exec', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    return res.status(200).json(result);
+
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
 }
